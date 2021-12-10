6bc1ef7c47e263399d1e2a6a6b6fdaec9d666371
jorge <jorge.arranz@gmail.com>
2021-12-10 17:21:06 +0100

    Control de errores en la creación del usuario

---
+++ database/config.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const dbConnection = async () => {
+  try {
+    await mongoose.connect(process.env.DB_CNN, {
+      useNewUrlParser: true,
+      /*useUnitfiedTopology: true,
+      useCreateIndex: true,*/
+    });
+
+    console.log("db online");
+  } catch (error) {
+    console.log(error);
+    throw new Error("Error a la hora de inicializar base de datos");
+  }
+};
+
+module.exports = {
+  dbConnection,
+};
